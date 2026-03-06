f37cb67d03c06628014d3a4d25e81b850d7fc9aa
RingkhangBTY <ringkhangb913@gmail.com>
2026-03-06 14:46:00 +0530

    Initial project set up1

---
+++ src/main/java/com/team_inertia/gonly/GonlyApplication.java
@@ -0,0 +1,11 @@
+package com.team_inertia.gonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GonlyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/team_inertia/gonly/GonlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_inertia.gonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
